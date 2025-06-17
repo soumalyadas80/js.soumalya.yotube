67cf84e77c3f81a279146ef34ddeb697e00548b8
Soumalya <126415989+soumalyadas80@users.noreply.github.com>
2025-06-17 15:20:53 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber)  // NaN means that not a number
+
+//"33" -> 33
+// "33abc"  -> NaN
+// true -> 1
+// false = 0
+  
+let isloggedIn = ""
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+// 1 -> true 
+// 0 -> false 
+// "" -> false 
+// "Soumalya" -> true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
