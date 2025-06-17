08dbd7334808c37d7872379f1db24a5a837f499c
Soumalya <126415989+soumalyadas80@users.noreply.github.com>
2025-06-17 16:58:37 +0000

    Remaining Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value 
+console.log(negValue);
+console.log(2*2);
+console.log(2+2);
+console.log(2-2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "hello"
+let str2 = " Soumalya"
+let str3 = str1 + str2 
+console.log(str3);
+console.log("hi" + " Ravi")
+console.log(1 + 2 +"2");
+console.log(1 + "2" + 2 );
+console.log("1" + 2 + 2 ); // ai output mai asa hua according to the guideline under ECMA Script
+console.log(1 + 3 * 4 % 3 + 2 ** 9);
+
+console.log(+true)
+// console.log(true+)  in this way we don't write the code
+
+let gameCounter =  100
+gameCounter++;
+console.log(gameCounter)
+
+// go & read precedence order from mdm web docs 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion   (Link for study purpose)
