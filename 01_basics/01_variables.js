5d64838c4edac3a2817703128107f7bf1a5c7df1
Soumalya <126415989+soumalyadas80@users.noreply.github.com>
2025-06-17 07:38:44 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144529
+let accountEmail = "soumalyadas80@gmail.com"
+var accountPassword = "Soumalya@2025"
+accountCity = "Kolkata"  // ase vi variable declare kar sakte ho
+let accountState;
+
+accountEmail = 2
+accountCity = "Bhupal"
+console.log(accountEmail);
+console.log(accountId);
+console.table([accountId , accountEmail , accountCity , accountPassword , accountState]);
+ /*
+  Prefer not to use var
+  because of use of block scope and functional scope in javascript.
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version 
+// alert(3+3)  we are using nodejs not browser  
+ // Javascript ka engine browser ke andar chupa hua hai
+
+ let name = "Soumalya"
+ let roll_no = 3
+ let isLoggedIn = true
+ let marks = 34.5
+ let nullVariable = null // null is a special value in javascript
+  // number => 2 to the power 53
+  // bigInt
+  // String = ""
+//boolean = true/false
+// null = empty value or standalone value
+//undefined = variables is not defined
+// Symbol = unique value
+
+//Object
+
+console.log(typeof "Soumalya");
+console.log(typeof null);
+console.log(typeof undefined);
