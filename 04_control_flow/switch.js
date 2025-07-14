85198e137404fb294b5049890da89d3ae9726bb2
Soumalya <126415989+soumalyadas80@users.noreply.github.com>
2025-07-14 17:31:04 +0000

    Switch Cases Day 2

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;    // cases banae ki liye we use [shift +Alt +Down-Arrow]
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+
+//     default:
+//         console.log("deafult case match");
+        
+//         break;
+// }
+
+const month = "january"
+switch (month) {
+    case "january":
+        console.log("January");
+        break;    // cases banae ki liye we use [shift +Alt +Down-Arrow]
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("deafult case match");
+        
+        break;
+}
+
