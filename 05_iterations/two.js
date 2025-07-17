61f1ccdcad76104acb5edaad0084bc48d8d64f35
Soumalya <126415989+soumalyadas80@users.noreply.github.com>
2025-07-17 14:47:28 +0000

    While & dowhile loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+// while
+
+// let index =0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index +2
+
+// } 
+
+// myArray le kar
+let myArray =['flash',"batman", "superman"]
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}` );
+    arr =arr+1;
+}
+
+//do while loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10); 
+// do while mai kam pahale hota hai baad mai condition check kia jata hai
+
+let score1 = 1
+do {
+    console.log(`Score1 is ${score1}`);
+    score++
+    
+} while (score1>=10); 
